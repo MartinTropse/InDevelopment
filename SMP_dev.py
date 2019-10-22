19d9b7f50c28467a87e2d61d418eb47419391a68
MartinTropse <53341120+MartinTropse@users.noreply.github.com>
2019-10-22 22:01:03 +0200

    Update SMP_dev.py

--- SMP_dev.py
+++ SMP_dev.py
@@ -9,48 +9,37 @@ Created on Tue Oct 22 14:41:06 2019
 
 from selenium import webdriver
 import selenium
-
 #from selenium.webdriver.common.keys import Keys
+import requests
+import pyautogui as auto
+import bs4 as bs 
+from selenium.webdriver.support.select import Select
 
-browser=webdriver.Chrome("C:\Py3\chromedriver.exe")
+browser=webdriver.Chrome("C:/Py3/chromedriver.exe")
+
+
+pas = ""
 mail="joakim.a.svensson@lansstyrelsen.se"
+url = "https://smp.lansstyrelsen.se/Tillsynsmyndighet/SearchAnlaggning.aspx"
+
+
+
+#browser=webdriver.Firefox(executable_path=r'C:\Py3\geckodriver.exe')    
 
 def loginFunc(password):
-    browser=webdriver.Chrome("C:\Py3\chromedriver.exe")    
-    browser.get("https://smp.lansstyrelsen.se/Tillsynsmyndighet/SearchAnlaggning.aspx")
+    browser=webdriver.Chrome("C:/Py3/chromedriver.exe")
+    browser.get(url)
     elemMail=browser.find_element_by_id("PageContent_ucLogin_txtUsername")
     elemMail.send_keys(mail)
     elemPass=browser.find_element_by_id("PageContent_ucLogin_txtPassword")
-    elemPass.send_keys(password)
+    elemPass.send_keys(pas)
     logButton=browser.find_element_by_name("ctl00$PageContent$ucLogin$btnLogin")
-    logButton.click()
+    logButton.click()  
+    return browser
 
 
-loginFunc("")
+lanElem=browser.find_element_by_id("_Miljörapport")
+lanElem.click()
 
-
-
-=browser.find_element_by_name=("ctl00$PageContent$ucSearchAnlaggning$ddlLan")
-
-
-<select name="ctl00$PageContent$ucSearchAnlaggning$ddlLan" id="PageContent_ucSearchAnlaggning_ddlLan" class="select1000_1" onchange="UpdateKommunList()" onkeydown="if( event.keyCode == 13 ) return Search();">
-	
-</option><option value="05">Östergötlands län</option></select>
-
-<select name="ctl00$PageContent$ucSearchAnlaggning$ddlLan" id="PageContent_ucSearchAnlaggning_ddlLan" class="select1000_1" onchange="UpdateKommunList()" onkeydown="if( event.keyCode == 13 ) return Search();">
-	
-
-
-
-
-
-
-	
-
-
-#browser=webdriver.Chrome("C:\Py3\chromedriver.exe")
-#browser.get("https://smp.lansstyrelsen.se/Tillsynsmyndighet/SearchAnlaggning.aspx")
-#
-#elem.click()
-#
-#<input type="submit" name="ctl00$PageContent$ucLogin$btnLogin" value="Logga in" id="PageContent_ucLogin_btnLogin" title="Logga in">
+obj = Select(browser.find_element_by_id('PageContent_ucSearchAnlaggning_ddlLan'))
+obj.select_by_value("05")
