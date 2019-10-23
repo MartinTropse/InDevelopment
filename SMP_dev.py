42fddea362881a303928855993626d739be72111
MartinTropse <53341120+MartinTropse@users.noreply.github.com>
2019-10-23 16:16:35 +0200

    Update SMP_dev.py

--- SMP_dev.py
+++ SMP_dev.py
@@ -5,8 +5,6 @@ Created on Tue Oct 22 14:41:06 2019
 @author: BigTail 
 """
 
-
-
 from selenium import webdriver
 import selenium
 #from selenium.webdriver.common.keys import Keys
@@ -18,11 +16,11 @@ from selenium.webdriver.support.select import Select
 browser=webdriver.Chrome("C:/Py3/chromedriver.exe")
 
 
-pas = ""
+
 mail="joakim.a.svensson@lansstyrelsen.se"
 url = "https://smp.lansstyrelsen.se/Tillsynsmyndighet/SearchAnlaggning.aspx"
 
-
+password = '!nneBandy22'
 
 #browser=webdriver.Firefox(executable_path=r'C:\Py3\geckodriver.exe')    
 
@@ -32,14 +30,29 @@ def loginFunc(password):
     elemMail=browser.find_element_by_id("PageContent_ucLogin_txtUsername")
     elemMail.send_keys(mail)
     elemPass=browser.find_element_by_id("PageContent_ucLogin_txtPassword")
-    elemPass.send_keys(pas)
+    elemPass.send_keys(password)
     logButton=browser.find_element_by_name("ctl00$PageContent$ucLogin$btnLogin")
     logButton.click()  
+    lanElem=browser.find_element_by_id("_Miljörapport")
+    lanElem.click()
     return browser
 
+browser=loginFunc(password)
 
-lanElem=browser.find_element_by_id("_Miljörapport")
-lanElem.click()
+kommunList = ["Boxholm", "Finspång", "Kinda","Linköping","Mjölby","Motala","Norrköping", "Söderköping","Vadstena", "Valdemarsvik", "Ydre", "Åtvidaberg", "Ödeshög"]
 
 obj = Select(browser.find_element_by_id('PageContent_ucSearchAnlaggning_ddlLan'))
 obj.select_by_value("05")
+objKom = Select(browser.find_element_by_id('PageContent_ucSearchAnlaggning_ddlKommun'))
+objKom.select_by_visible_text("Linköping")
+
+elemShow=browser.find_element_by_id("btnSearch")
+elemShow.click()
+
+browser.find_element_by_link_text()
+
+s = requests.session()
+a=s.get(url)
+c=a.text
+
+
